8bb42298916f7b28fc55c735468f644bb56a8265
quarzasiphix <76133968+quarzasiphix@users.noreply.github.com>
2025-12-19 17:28:45 +0100

    latest

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
+
+  // Generate pure static output for Cloudflare Pages
+  output: "export",
+  images: { unoptimized: true },
+  trailingSlash: false,
+
   async rewrites() {
     return [
       {
-        source: '/app/:path*',
-        destination: 'https://app.ksiegai.pl/:path*',
+        source: "/app/:path*",
+        destination: "https://app.ksiegai.pl/:path*",
       },
     ];
   },
